04c25bf5b035e6d5b655d10d47225977305447c7
Anastasia Lucas <ani@theta.local>
2017-12-05 18:20:43 -0500

    modules

--- config/orm.js
+++ config/orm.js
@@ -1,17 +1,25 @@
-var connection = require("/connection.js");
+var connection = require("./connection.js");
 
 const orm = {
 selectAll(){
-
-}
+//get table name from module requested from controller
+//run a query, get data, return it to module
+// module returns it back to controller
+},
 
 insertOne(){
+//we get a value to insert from contoller?
+//pass the value to sql
 
-}
+
+},
 
 updateOne(){
+//check everything , console log it
 
-}
+},
+
+
 }
 
 module.exports = orm;
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,3 +1,19 @@
 var express = require("express");
 
 var burgers = require("../models/burger.js");
+
+var router = express.Router();
+
+//get all burgers
+//send request to module which sends it to orm
+//orm returns via module, module returns it back to controller to be rendered
+router.get('/', (req, res){
+
+    burgers.selectAll()
+});
+
+
+
+
+
+module.exports = router;
