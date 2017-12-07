8b2d073796033a6b9ea928eabd013bff7912fda5
Anastasia Lucas <ani@theta.local>
2017-12-06 19:35:03 -0500

    models

--- models/burger.js
+++ models/burger.js
@@ -1,9 +1,27 @@
 var orm = require("../config/orm.js");
 
 //code to call the orm functions using burger specific input
+var burgers = {
+  all: function(cb){
+    orm.selectAll("burgers", function(res){
+      cb(res);
+    });
+  },
 
+  create: function(col, val, cb){
+    orm.insertOne("burgers", col, val, function(res){
+      cb(res);
+    });
+  },
+
+  update: function(vals, condition, cb){
+    orm.updateOne("burgers", vals, condition, function(res){
+      cb(res);
+    });
+  }
+};
 
 
 //export this code
 
-module.exports = ;
+module.exports = burgers;
